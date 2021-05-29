be765e2fa909446d00389dfb6f6ee0388008e7fb
Zoe Kim <zoe74250921@gmail.com>
2021-05-29 15:44:04 +0900

    스킬 이미지 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,18 @@
 		$('.scrolly').scrolly();
 
 })(jQuery);
+
+  /* Demo purposes only */
+var snippet = [].slice.call(document.querySelectorAll('.hover'));
+if (snippet.length) {
+  snippet.forEach(function (snippet) {
+    snippet.addEventListener('mouseout', function (event) {
+      if (event.target.parentNode.tagName === 'figure') {
+        event.target.parentNode.classList.remove('hover')
+      } else {
+        event.target.parentNode.classList.remove('hover')
+      }
+    });
+  });
+}
+
